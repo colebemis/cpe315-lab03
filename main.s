5220704cfddafed45593fd0e0dd41f64ec408f5a
Cole Bemis <colebemis@gmail.com>
2018-04-18 20:59:52 -0700

    Implement input loop

--- main.s
+++ main.s
@@ -7,9 +7,10 @@ main:
   /* store lr on stack */
   str lr, [sp, #-4]!
 
-  /* make space for inputs (2 x 4 bytes) */
-  sub sp, sp, #8
+  /* make space for inputs */
+  sub sp, sp, #16
 
+loop:
   /* print prompt for number 1 */
   ldr r0, =prompt1
   bl  printf
@@ -45,12 +46,32 @@ main:
   ldr r0, =result
   bl printf
   */
-  
+
+  /* print again prompt */
+  ldr r0, =again
+  bl printf
+
+  /* read char */
+  ldr r0, =readchar
+  add r1, sp, #12
+  bl scanf
+
+  /* put 'y' in r1 */
+  ldr r1, =yes
+  ldrb r1, [r1]
+
+  /* put answer in r0 */
+  ldrb r0, [sp, #12]
+
+  /* compare r0 and r1 */
+  cmp r0, r1
+  beq loop
+
   /* set return value to 0 */
   mov r0, #0
 
   /* restore stack */
-  add sp, sp, #8
+  add sp, sp, #16
 
   /* load pc with lr */
   ldr pc, [sp], #4
@@ -65,10 +86,12 @@ prompt3:
 readint:
   .asciz "%d"
 readchar:
-  .asciz "%c"
+  .asciz " %c"
 result:
   .asciz "Result is: %d\n"
 again:
   .asciz "Again? "
 invalid:
   .asciz "Invalid Operation Entered.\n"
+yes:
+  .byte 'y'
